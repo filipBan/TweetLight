67dedc466b38d31d8fd33846b00028d900882d65
filip.banasiak <filip.banasiak@matillion.com>
2022-08-27 09:23:53 +0100

    switch to google auth provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth'
-import TwitterProvider from 'next-auth/providers/twitter'
+import GoogleProvider from 'next-auth/providers/google'
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
@@ -23,10 +23,11 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    TwitterProvider({
-      clientId: env.TWITTER_CLIENT_ID,
-      clientSecret: env.TWITTER_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
+
     // ...add more providers here
   ],
 }
